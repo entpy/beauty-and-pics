9439b6cb786a43369c13416b987ce3d4badf65af
developer <developer@entpy.com>
2015-01-18 17:34:02 +0100

    integrate bootstrap in django template

--- beauty_and_pics/beauty_and_pics/urls.py
+++ beauty_and_pics/beauty_and_pics/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'beauty_and_pics.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^index/$', 'website.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
