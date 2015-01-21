9b3c9fcc992701001917a8c0fb6ca57ce9f6112e
developer <developer@entpy.com>
2015-01-21 18:28:25 +0100

    playing with django template system

--- beauty_and_pics/beauty_and_pics/urls.py
+++ beauty_and_pics/beauty_and_pics/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^index/$', 'website.views.index', name='index'),
-    url(r'^come-funziona/$', 'website.views.come_funziona', name='come_funziona'),
+    url(r'^come-funziona/$', 'website.views.how_it_works', name='how_it_works'),
     url(r'^login/$', 'website.views.login', name='login'),
     url(r'^recupera-password/$', 'website.views.forgot_password', name='forgot_password'),
     url(r'^registrati/$', 'website.views.register', name='register'),
--- beauty_and_pics/website/views.py
+++ beauty_and_pics/website/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'website/index.html', False)
 
-def come_funziona(request):
+def how_it_works(request):
     return render(request, 'website/come_funziona.html', False)
 
 def login(request):
