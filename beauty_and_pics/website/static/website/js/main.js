59a6a7407f9950aedad111e14c6cf1f581cfa0aa
developer <developer@entpy.com>
2015-02-13 16:55:47 +0100

    working on mobile view

--- beauty_and_pics/website/static/website/js/main.js
+++ beauty_and_pics/website/static/website/js/main.js
@@ -1,29 +1,35 @@
-$.fn.masonryImagesReveal = function( $items ) {
+$(document).ready(function(){
+	$(".navbar-toggle").on("click", function(){
+		$(".toggle_navigation").toggle();
+	});
+});
+
+$.fn.masonryImagesReveal = function($items) {
 	var msnry = this.data('masonry');
 	var itemSelector = msnry.options.itemSelector;
 	// hide by default
 	$items.hide();
 	// append to container
-	this.append( $items );
-	$items.imagesLoaded().progress( function( imgLoad, image ) {
+	this.append($items);
+	$items.imagesLoaded().progress(function(imgLoad, image) {
 		// get item
 		// image is imagesLoaded class, not <img>, <img> is image.img
-		var $item = $( image.img ).parents( itemSelector );
+		var $item = $(image.img).parents(itemSelector);
 		// un-hide item
 		$item.show();
 		// masonry does its thing
-		msnry.appended( $item );
+		msnry.appended($item);
 	});
 
 	return this;
 };
 
-function randomInt( min, max ) {
-	return Math.floor( Math.random() * max + min );
+function randomInt(min, max) {
+	return Math.floor(Math.random() * max + min);
 }
 
 function getItem() {
-	var format = randomInt( 150, 250 );
+	var format = randomInt(150, 250);
 	var width = format;
 	var height = format;
 	var item = '<div class="item"><img src="http://lorempixel.com/' + width + '/' + height + '/nature" /></div>';
@@ -35,6 +41,7 @@ function getItems() {
 	for ( var i=0; i < 12; i++ ) {
 		items += getItem();
 	}
+
 	// return jQuery object
-	return $( items );
+	return $(items);
 }
