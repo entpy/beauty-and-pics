4d5200e417ded79136fbdcc752de0da0dd1374b2
developer <developer@entpy.com>
2015-02-18 18:35:18 +0100

    working on birthday date

--- beauty_and_pics/website/forms.py
+++ beauty_and_pics/website/forms.py
@@ -4,35 +4,46 @@ import calendar
 
 class FormCommonUtils():
     def get_days_select_choices(self):
-            """Create a list of days for select element"""
-            select_choices = []
-            for i in range(1,32):
-                    select_choices.append((i, i))
-            return select_choices
+        """Create a list of days for select element"""
+        select_choices = []
+        for i in range(1,32):
+                select_choices.append((i, i))
+        return select_choices
 
     def get_months_select_choices(self):
-            """Create a list of months for select element"""
-            select_choices = []
-            for i in range(1,13):
-                    select_choices.append((i, calendar.month_name[i]))
-            return select_choices
+        """Create a list of months for select element"""
+        select_choices = []
+        for i in range(1,13):
+                select_choices.append((i, calendar.month_name[i]))
+        return select_choices
 
     def get_years_select_choices(self):
-            """Create a list of years for select element"""
-            #                   current year - 18
-            #                           |
-            #                           V
-            select_choices = []
-            for i in range(1960, (date.today().year - 17)):
-                    select_choices.append((i, i))
-            return select_choices
+        """Create a list of years for select element"""
+        #                   current year - 18
+        #                           |
+        #                           V
+        select_choices = []
+        for i in range(1960, (date.today().year - 17)):
+                select_choices.append((i, i))
+        return select_choices
 
     def get_genders_select_choices(self):
-            """Create a list of genders for select element"""
-            select_choices = []
-            select_choices.append(("f", "Donna"))
-            select_choices.append(("m", "Uomo"))
-            return select_choices
+        """Create a list of genders for select element"""
+        select_choices = []
+        select_choices.append(("f", "Donna"))
+        select_choices.append(("m", "Uomo"))
+        return select_choices
+
+    def check_if_user_is_adult(self, birthday_dictionary):
+        """Check if a user is adult or not.
+        Return: true on success, false otherwise"""
+        birthday_year = birthday_dictionary.get("birthday_year")
+        birthday_month = birthday_dictionary.get("birthday_month")
+        birthday_day = birthday_dictionary.get("birthday_day")
+
+        if birthday_year and birthday_month and birthday_day:
+            # date(yy/mm/dd)
+            birthday_date = date(birthday_year, birthday_month, birthday_day)
 
 class RegisterForm(forms.Form):
 
