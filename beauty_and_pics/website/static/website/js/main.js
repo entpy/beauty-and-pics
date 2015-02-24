d4411e372a84f9d65fa999191e9e049ab86826d0
developer <developer@entpy.com>
2015-02-24 18:20:22 +0100

    working on image grid and css fix

--- beauty_and_pics/website/static/website/js/main.js
+++ beauty_and_pics/website/static/website/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	});
 });
 
-$.fn.masonryImagesReveal = function($items) {
+/*$.fn.masonryImagesReveal = function($items) {
 	var msnry = this.data('masonry');
 	var itemSelector = msnry.options.itemSelector;
 	// hide by default
@@ -22,24 +22,35 @@ $.fn.masonryImagesReveal = function($items) {
 	});
 
 	return this;
-};
+};*/
 
 function randomInt(min, max) {
 	return Math.floor(Math.random() * max + min);
 }
 
-function getItem() {
+function getItem(block_size) {
+
+	if (typeof block_size === 'undefined') {
+		block_size = true;
+	}
+
+	// setting bootstrap block size
+	var lg_size = (block_size["lg_size"] ? block_size["lg_size"] : "15");
+	var md_size = (block_size["md_size"] ? block_size["md_size"] : "3");
+	var sm_size = (block_size["sm_size"] ? block_size["sm_size"] : "3");
+	var xs_size = (block_size["xs_size"] ? block_size["xs_size"] : "6");
 	var format = randomInt(150, 250);
 	var width = format;
 	var height = format;
-	var item = '<div class="item"><img src="http://lorempixel.com/' + width + '/' + height + '/nature" /></div>';
+	var item = '<div class="col-lg-' + lg_size + ' col-md-' + md_size + ' col-xs-' + xs_size + ' col-sm-' + sm_size + ' thumb"><a href="#" class="thumbnail"><img alt="" src="http://lorempixel.com/' + width + '/' + height + '/nature" class="img-responsive"></a></div>'
+
 	return item;
 }
 
-function getItems() {
+function getItems(block_size) {
 	var items = '';
 	for ( var i=0; i < 12; i++ ) {
-		items += getItem();
+		items += getItem(block_size);
 	}
 
 	// return jQuery object
