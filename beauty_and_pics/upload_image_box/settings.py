41f2d6ae7eaeff9469b58ac02868089b571689cf
developer <developer@entpy.com>
2015-04-19 23:18:20 +0200

    working on images upload app, added attr on widget, loading page, upload view without crop

--- beauty_and_pics/upload_image_box/settings.py
+++ beauty_and_pics/upload_image_box/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 APP_BASE_DIRECTORY = 'upload_image_box/'
 UPLOADED_IMG_TMP_DIRECTORY = 'tmp_upload/'
 CROPPED_IMG_DIRECTORY = 'cropped_upload/'
-CUSTOM_CROPPED_IMG_DIRECTORY = 'user_12/' # ex. "test/"
+CUSTOM_CROPPED_IMG_DIRECTORY = '' # ex. "user_12/"
 CROPPED_IMG_MIN_WIDTH = 200
 CROPPED_IMG_MIN_HEIGHT = 200
 
@@ -26,7 +26,7 @@ TODO
 
 - V Spostare le immagini croppate nella directory CROPPED_IMG_DIRECTORY
 
-- Eliminare le immagini nella cartella UPLOADED_IMG_TMP_DIRECTORY dopo un tot di tempo (capire come fare)
+- Eliminare le immagini nella cartella UPLOADED_IMG_TMP_DIRECTORY e dal db temporaneo dopo un tot di tempo (capire come fare)
 
 - Capire dove e come inserire i seguenti parametri del widget:
 	V i testi (dei pulsanti e dei titoli)
@@ -34,6 +34,7 @@ TODO
 	  la larghezza/altezza minima dell'immagine croppata
 	V se abilitare o no il crop dell'immagine (saltare alla pagina di riepilogo o mostrare il crop)
 	  l'html del widget
+- Modificare $('#upload_image_box_modal') in una classe per poter utilizzare più loader nella stessa pagina
 
 Per inserire le immagini in una directory custom inserire il nome nell'impostazione del widget e nella view 
 che genera il widget per l'upload. In questo modo sono in grado di controllare che non avvengano caricamenti
