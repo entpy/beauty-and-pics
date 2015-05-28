5d98b8e0702943a390d1af174026b3cf2cce5fba
developer <developer@entpy.com>
2015-05-28 23:01:32 +0200

    working on image upload

--- beauty_and_pics/upload_image_box/widgets.py
+++ beauty_and_pics/upload_image_box/widgets.py
@@ -81,6 +81,8 @@ class UibUploaderInput(forms.ClearableFileInput):
         parent_validation = super(UibUploaderInput, self).value_from_datadict(data, files, name)
         # logger.debug("files retrieved: " + str(files))
         file_object = files.get('image', None)
+        logger.debug("files: " + str(files))
+        logger.debug("data: " + str(data))
         logger.debug("file object: " + str(file_object))
         if parent_validation is not None and file_object is not None:
             # check image MIME/Type
