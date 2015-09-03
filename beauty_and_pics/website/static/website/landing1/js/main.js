ba7baefe85c857f10de3421b9b557b2bb30ea6bb
developer <developer@entpy.com>
2015-09-03 11:20:47 +0200

    working on landing1

---
+++ beauty_and_pics/website/static/website/landing1/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$(window).load(function(){
+		$('.page-loader').fadeOut('slow');
+	});
+
+	wow = new WOW({
+		animateClass: 'animated',
+		mobile: false,
+		offset: 70
+	});
+	wow.init();
+});
