adbaa814fd3e9ebcfc6921ad67e8e2a136e4acb9
developer <developer@entpy.com>
2015-09-11 18:16:21 +0200

    working on notify system

--- beauty_and_pics/notify_system_app/models.py
+++ beauty_and_pics/notify_system_app/models.py
@@ -6,6 +6,8 @@ from datetime import date
 from dateutil.relativedelta import *
 from website.exceptions import *
 from beauty_and_pics.consts import project_constants
+from email_template.email.email_template import *
+from account_app.models.accounts import Account
 import sys, logging
 
 # force utf8 read data
@@ -86,7 +88,7 @@ class Notify(models.Model):
     def get_checkbox_dictionary(self, paginator_element_list=False, checked_elements=False):
             """
             Function to render a senders dictionary like this:
-            { "4" : 1, "5" : 1, "6" : 0, "7" : 1, "8" : 0 }
+            { "mail1@mail.com" : 1, "mail2@mail.com" : 1, "mail3@mail.com" : 0, "mail4@mail.com" : 1, "mail5@mail.com" : 0 }
             Return a checkbox status dictionary on success
 
             paginator_element_list: all objects in current view (list)
@@ -99,10 +101,10 @@ class Notify(models.Model):
             for element in paginator_element_list:
                 # logger.debug("get_checkbox_dictionary, element: " + str(element.user.id))
                 # se element è contenuto in checked_elements allora ok
-                if (str(element.user.id) in checked_elements):
-                    checkbox_dictionary[element.user.id] = 1
+                if (str(element.user.email) in checked_elements):
+                    checkbox_dictionary[element.user.email] = 1
                 else:
-                    checkbox_dictionary[element.user.id] = 0
+                    checkbox_dictionary[element.user.email] = 0
 
             # logger.error("models.py, get_senders_dictionary: " + str(checkbox_dictionary))
             logger.debug("get_checkbox_dictionary, checkbox_dictionary: " + str(checkbox_dictionary))
@@ -121,13 +123,83 @@ class Notify(models.Model):
                 for key, value in request.session.get('checked_users').iteritems():
                     if value:
                         logger.debug("[get_account_list], single_elements: key=" + str(key) + " value=" + str(value))
-                        return_var.append(int(key))
+                        return_var.append(str(key))
 
             return return_var
 
-    # function to send a notify to a recipents list
-    def send_notify_via_mail(self, notify_id, recipients_list):
+    def save_checked_elemets(self, paginator, request = {}):
+        """Function to save into session checked elements from djago paginator"""
+        notify_obj = Notify()
+        # previously page number
+        old_viewed_page = request.POST.get('current_page', 1)
+
+        # add/remove id into session
+        checked_contacts = request.POST.getlist('contacts[]')
+
+        # retrieving checked list from current view (only checkbox that are shown from paginator current view)
+        senders_dictionary = notify_obj.get_checkbox_dictionary(paginator.page(old_viewed_page), checked_contacts)
+
+        # saving or removing checked/unchecked checkbox from db
+        checked_users = notify_obj.set_campaign_user(senders_dictionary=senders_dictionary, request=request)
+
+        return checked_users
+
+    def build_form_initial_value(self, request, kwargs = {}):
+        """Function to build admin form initial value dictionary"""
+        notify_obj = Notify()
+        notify_retrieved = {}
+        try:
+            if int(kwargs.get('notify_id') or 0):
+                # prelevo i dati della notifica per precompilarla nel form
+                notify_retrieved = notify_obj.get_notify_info(notify_id=kwargs.get('notify_id'))
+        except ValueError:
+            pass
+
+	return_var = {
+	    "title": request.POST.get('title', notify_retrieved.get('title')),
+	    "message": request.POST.get('message', notify_retrieved.get('message')),
+	    "action_url": request.POST.get('action_url', notify_retrieved.get('action_url')),
+	}
+
+        return return_var
+
+
+    def send_notify_via_mail(self, notify_data, recipients_list):
+        """function to send a custom notify to a recipents list"""
+        email_context = {
+            "title" : str(notify_data.get('title')),
+            "message" : str(notify_data.get('message')),
+            "action_url": str(notify_data.get('action_url')),
+        }
+
+        # send notify email with custom email template object app ;)
+        CustomEmailTemplate(
+            email_name="custom_notify_email",
+            email_context=email_context,
+            template_type="user",
+            recipient_list=[recipients_list,]
+        )
+
         return True
+
+    def create_recipients_list(self, retrieve_all_users = False, campaign_contacts_list = [], addictional_email_string = ''):
+        """Function to create a recipients list"""
+        return_var = []
+        addictional_email_list = []
+
+        # TODO fare qui
+        if retrieve_all_users:
+            all_contacts_list = Account.objects.values('user__email').filter(user__groups__name=project_constants.CATWALK_GROUP_NAME)
+
+        # retrieve addictional email recipients
+        if addictional_email_string:
+            for element in addictional_email_string.split(';'):
+                addictional_email_list.append(element)
+
+        # concat selected emails with addictional emails
+        recipients_list = campaign_contacts_list + addictional_email_list
+
+        return return_var
 
     # function to mark a notify as read
     def mark_notify_as_read(self, notify_id, user_id):
@@ -139,7 +211,20 @@ class Notify(models.Model):
 
     # function to retrieve info (title, description, ecc...) about a notify id
     def get_notify_info(self, notify_id):
-        return True
+        return_var = {}
+
+        try:
+            notify_obj = Notify.objects.get(pk=notify_id)
+        except Notify.DoesNotExist:
+            pass
+        else:
+            return_var['notify_id'] = notify_obj.notify_id
+            return_var['creation_date'] = notify_obj.creation_date
+            return_var['title'] = notify_obj.title
+            return_var['message'] = notify_obj.message
+            return_var['action_url'] = notify_obj.action_url
+
+        return return_var
 
     # function to retrieve a list of notify about a user
     def user_notify_list(self, filters, user_id):
