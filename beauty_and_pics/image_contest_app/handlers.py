543006a82dc78936758f3ba9106d19985afba173
developer <developer@entpy.com>
2015-11-20 01:16:53 +0100

    delete winner photoboard image, working on signals and photoboard all of fame

--- beauty_and_pics/image_contest_app/handlers.py
+++
@@ -1,8 +0,0 @@
-"""
-# -*- coding: utf-8 -*-
-from django.dispatch import receiver
-
-def post_delete_callback(sender, instance, using, **kwargs):
-    # se il concorso relativo all'immagine è chiuso lo metto in stato scaduto
-    logger.info("--Elemento rimosso con successo: " + str(instance))
-"""
