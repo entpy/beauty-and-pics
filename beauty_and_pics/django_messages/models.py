7b960e9f474b36d73c1b034699fbf1f8a73af574
developer <developer@entpy.com>
2016-06-27 18:36:22 +0200

    working on django message

--- beauty_and_pics/django_messages/models.py
+++ beauty_and_pics/django_messages/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
+from django.db.models import Q
 from datetime import timedelta
 from django.utils import timezone
 import logging
@@ -8,15 +9,57 @@ import logging
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-class Message(models.Model):
-    id_message = models.AutoField(primary_key=True)
-    sender = models.ForeignKey(User) # mittente del messaggio
-    recipient = models.ForeignKey(User) # destinatario del messaggio
-    text = models.TextField() # testo del messaggio
-    write_date = models.DateTimeField(auto_now=True)
-    read_date = models.DateTimeField(null=True, blank=True) # se non settato il messaggio non è stato letto
+class Conversation(models.Model):
+    id_conversation = models.AutoField(primary_key=True)
+    master_user = models.ForeignKey(User) # creatore della conversazione
+    partecipant = models.ForeignKey(User) # partecipante della conversazione
     sender_deleted_at = models.DateTimeField("cancellato dal mittente il", null=True, blank=True)
     recipient_deleted_at = models.DateTimeField("cancellato dal destinatario il", null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now=True) # la data di creazione della conversazione
+
+    class Meta:
+        app_label = 'django_messages'
+
+    def __unicode__(self):
+        return str(self.id_conversation)
+
+    def get_conversation(self, user_id1, user_id2):
+        """Function to retrieve a conversation instance"""
+        return_var = None
+
+        try:
+            return_var = Conversation.objects.get(Q('sender__id=' + user_id1, 'recipient__id=' + user_id2) | Q('sender__id=' + user_id2, 'recipient__id=' + user_id1))
+        except Conversation.DoesNotExist:
+            raise
+
+        return return_var
+
+    def create_conversation(self, sender_id, recipient_id):
+        """Function to create a new conversation between two users"""
+        return_var = None
+
+        Conversation_obj = Conversation()
+        Conversation_obj.sender_id = sender_id
+        Conversation_obj.recipient_id = recipient_id
+        if Conversation_obj.save():
+            return_var = Conversation_obj
+
+        return return_var
+
+    def get_user_conversations(self, user_id):
+        """Function to retrieve a conversation list about a user (in genere
+        l'utente loggato)"""
+        return_var = None
+
+        return list(Conversation.objects.values('sender__id', 'recipient__id').filter(Q('sender__id=' + user_id) | Q('recipient__id=' + recipient_id)))
+
+
+class Message(models.Model):
+    id_message = models.AutoField(primary_key=True)
+    conversation = models.ForeignKey(Conversation)
+    message = models.TextField() # testo del messaggio
+    write_date = models.DateTimeField(auto_now=True)
+    read_date = models.DateTimeField(null=True, blank=True) # se non settato il messaggio non è stato letto
     sender_is_blocked = models.IntegerField(default=0) # se 1 il mittente del messaggio è bloccato (quindi il messaggio non deve essere visibile al destinatario)
 
     class Meta:
@@ -25,22 +68,51 @@ class Message(models.Model):
     def __unicode__(self):
         return str(self.id_message)
 
-    def write_message(self, sender_id, recipient_id, text):
-        """Function to write a new message"""
+    def add_message_to_conversation(self, sender_id, recipient_id, message):
+        """Function to add a message to a conversation between two users"""
+        return_var = False
+        Conversation_obj = Conversation()
         UserBlock_obj = UserBlock()
         Message_obj = Message()
 
-        # controllo se il destinatario ha bloccato il mittente
+        # prelevo la conversazione tra i due (non so chi tra i due sia il
+        # mittente e chi il destinatario)
+        try:
+            Message_obj.conversation = Conversation_obj.get_conversation(user_id1=sender_id , user_id2=recipient_id)
+        except Conversation.DoesNotExist:
+            # la conversazione tra i due non esiste, quindi la creo
+            Message_obj.conversation = Conversation_obj.create_conversation(sender_id=sender_id, recipient_id=recipient_id)
+
+        # controllo che il destinatario non abbia bloccato il mittente
         if UserBlock_obj.check_if_user_is_blocked(user_id=recipient_id, user_blocked_id=sender_id):
             # il mittente è stato bloccato
             Message_obj.sender_is_blocked = 1
 
-        Message_obj.sender_id = sender_id
-        Message_obj.recipient_id = recipient_id
-        Message_obj.text = text
-        Message_obj.save()
+        Message_obj.message = message
+        if Message_obj.save():
+            return_var = True
 
-        return True
+        return return_var
+
+    def get_conversation_messages(self, user_id1, user_id2):
+        """Function to retrieve all conversation messages about two users"""
+        return_var = None
+
+        if user_id1 and user_id2:
+            return_var = list(Message.objects.values(
+                'conversation__sender__id',
+                'conversation__sender__first_name',
+                'conversation__sender__last_name',
+                'conversation__recipient__id',
+                'conversation__recipient__first_name',
+                'conversation__recipient__last_name',
+                'message',
+                'write_date'
+            ).filter(
+                Q('sender__id=' + user_id) | Q('recipient__id=' + recipient_id)
+            ).order_by('-write_date'))
+
+        return return_var
 
     def read_messages(self, sender_id, recipient_id):
         """
@@ -72,14 +144,6 @@ class Message(models.Model):
 
         return True
 
-    def get_messages_about_sender(self, sender_id):
-        """
-            Function to retrieve all undeleted and 
-            unblocked (sender_is_blocked = 0) message about a sender
-        """
-
-        return True
-
 
 class UserBlock(models.Model):
     id_user_block = models.AutoField(primary_key=True)
@@ -87,27 +151,38 @@ class UserBlock(models.Model):
     user_blocked = models.ForeignKey(User) # l'utente che è stato bloccato
     date = models.DateTimeField(auto_now=True) # la data in cui è avvenuto il blocco
 
-
     class Meta:
         app_label = 'django_messages'
 
     def __unicode__(self):
         return str(self.id_user_blocked)
 
-    def block_user(self):
+    def block_user(self, user_id, user_blocked_id):
         """Function to block a user"""
+        return_var = False
+        UserBlock_obj = UserBlock()
 
-        return True
+        UserBlock_obj.user_id = user_id
+        UserBlock_obj.user_blocked_id = user_blocked_id
+        if UserBlock_obj.save():
+            return_var = True
 
-    def unblock_user(self):
-        """Function to block a user"""
+        return return_var
+
+    def unblock_user(self, user_id, user_blocked_id):
+        """Function to un-block a user"""
+        if UserBlock.objects.filter('user__id=' + user_id, 'user_blocked__id=' + user_blocked_id).delete()
 
         return True
 
     def check_if_user_is_blocked(self, user_id, user_blocked_id):
         """Function to check if a user is blocked"""
+        return_var = False
 
-        return True
+        if UserBlock.objects.filter('user__id=' + user_id, 'user_blocked__id=' + user_blocked_id).exists():
+            return_var = True
+
+        return return_var
 
     def show_unblocked_recipients(self):
         """Function to show all unblocked recipients"""
